c344a6ed81f88a50443abbda7d1a8a770d0c0248
Andres Bedoya <and1res@hotmail.com>
2025-03-10 10:56:44 -0500

    add: Componentes Event Section

---
+++ src/components/pages/Info.jsx
@@ -0,0 +1,25 @@
+function Info()
+{
+    return (<>
+    <section id="info">
+      <h1>Información sobre el evento</h1>
+      <p class="fecha">Fecha: Jueves 14 de noviembre de 2024</p>
+      <p>
+        El evento se realizará en el campus laureles de la Universidad Pontificia Bolivariana de Medellín (clic
+        <a href="https://www.upb.edu.co/es/universidad/nuestro-campus/campus-medellin/laureles/como-llegar" target="_blank">
+          aquí</a>
+        para ver como llegar).
+      </p>
+      <p class="fecha">
+        Lugar: Aula 616, Bloque 11 Ingenierías.
+      </p>
+      <p class="fecha">
+        Hora: 17:30 - 20:30.
+      </p>
+      <span class="mensaje_importante">
+        * Traer documento de identidad para poder ingresar a la universidad.
+      </span>
+    </section>
+    </>);
+}
+export default Info;
---
+++ src/components/pages/PastEvent.jsx
@@ -0,0 +1,34 @@
+function PastEvent(){
+    return (<>
+      <section>
+      <h2>Eventos anteriores</h2>
+      <ul class="lista_generica">
+        <li>
+          🌱<a href="archivo/01/" target="_blank">CWF - CONF (el inicio)</a>
+        </li>
+        <li>
+          🍼<a href="archivo/02/" target="_blank">CWF - Renacimiento</a>
+        </li>
+        <li>
+          🎙️<a href="archivo/03/" target="_blank">CWF - Día de la mujer</a>
+        </li>
+        <li>
+          🕵️ <a href="archivo/04/" target="_blank">CWF - Hackerone</a>
+        </li>
+        <li>
+          🕹️ <a href="archivo/05/" target="_blank">CWF - VR / Touchdesigner</a>
+        </li>
+        <li>
+          🖲️ <a href="archivo/06/" target="_blank">CWF - Design / WebGL</a>
+        </li>
+        <li>
+          🏛️ <a href="archivo/07/" target="_blank">CWF - G.D.S. / Hacking Interviews</a>
+        </li>
+        <li>
+          ⚔️ <a href="archivo/08/" target="_blank">CWF - React vs Angular</a>
+        </li>
+      </ul>
+    </section>
+    </>);
+}
+export default PastEvent;
